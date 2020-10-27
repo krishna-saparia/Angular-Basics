95bd9811bd02fe1d1c5c6c6c4db8c8c3c22df583
krishna saparia <krishnasaparia43045@gmail.com>
2020-10-27 14:12:38 -0400

    First app
    use form and in field area typing name and giving typing output

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'my-first-angular-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-first-angular-app');
+    expect(app.name).toEqual('my-first-angular-app');
   });
 
   it('should render title', () => {
